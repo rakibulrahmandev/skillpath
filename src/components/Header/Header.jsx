139ec4d75ab0686b912495ba43b7cdefdf943b7f
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-26 11:51:17 +0600

    header design done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,25 +5,25 @@ export default function Header() {
     return (
         <>
             <header className="w-full py-5">
-                <div className="container">
+                <div className="container flex items-center justify-between border-b border-b-white-95 pb-5">
                     <div className="w-auto">
                         <a href="#" >
                             <img src="/image/logo.png" alt="logo" width={170} height={50} />
                         </a>
                     </div>
                     {/* sm nav */}
-                    <nav className="lg:hidden">
-                        <button>
+                    <nav className="lg:hidden fixed top-0 left-0 bg-white w-full h-screen flex flex-col items-center justify-center z-40 -translate-y-full transition-transform">
+                        <button className="absolute top-8 right-8">
                             <RiCloseLine size={30}/>
                         </button>
-                        <ul className="">
+                        <ul className="space-y-5 text-center">
                             {navItems.map((item) => (
                                 <li key={item?.id}>
-                                    <a href="#">{item?.label}</a>
+                                    <a href="#" className="text-lg font-medium hover:text-orange-50 transition-colors">{item?.label}</a>
                                 </li>
                             ))}
                         </ul>
-                        <button className="">Login</button>
+                        <button className="primary-btn mt-12 max-w-40 w-full">Login</button>
                     </nav>
                     <div className="w-auto lg:hidden">
                         <button className="">
@@ -31,15 +31,15 @@ export default function Header() {
                         </button>
                     </div>
                     {/* lg nav */}
-                    <div className="max-lg:hidden">
-                        <ul className="">
+                    <div className="max-lg:hidden flex items-center gap-10">
+                        <ul className="flex gap-10">
                             {navItems.map((item) => (
                                 <li key={item?.id}>
-                                    <a href="#">{item?.label}</a>
+                                    <a href="#" className="hover:text-orange-50 transition-colors font-medium text-lg">{item?.label}</a>
                                 </li>
                             ))}
                         </ul>
-                        <button className="max-lg:hidden">Login</button>
+                        <button className="max-lg:hidden primary-btn">Login</button>
                     </div>
                 </div>
             </header>
