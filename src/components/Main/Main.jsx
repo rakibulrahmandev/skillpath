3a4ce10c9e5fab2fe58d65ac030447a7f617a4f2
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-27 12:05:24 +0600

    create Benefits.jsx file and mount this component in Main.jsx file

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
+import Benefits from "../Benefits/Benefits";
 import Home from "../Home/Home";
 
 export default function Main() {
     return (
         <>
             <Home />
+            <Benefits />
         </>
     );
 };
