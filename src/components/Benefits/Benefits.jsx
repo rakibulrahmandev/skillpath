cf010376c1f044d42f3789e9d6e812000ae3bac3
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-27 12:26:31 +0600

    Benefits.jsx file html structure done

--- src/components/Benefits/Benefits.jsx
+++ src/components/Benefits/Benefits.jsx
@@ -1,7 +1,31 @@
+import { RiArrowRightUpLine } from "@remixicon/react";
+import { benefitItems } from "../../constants/data";
+import Title from "../Title/Title";
+
 export default function Benefits() {
     return (
         <>
-            
+            <section className="section">
+                <div className="container">
+                    <Title title='Benefits' text='Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.' link='View All' />
+                    <div className="">
+                        {benefitItems.map((item) => (
+                            <div className="">
+                                <div className="">
+                                    <img src={item?.icon} alt={item?.title} width={64} height={64} />
+                                </div>
+                                <div className="">
+                                    <h4>{item?.title}</h4>
+                                    <p>{item?.text}</p>
+                                </div>
+                                <button>
+                                    <RiArrowRightUpLine size={26} />
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
         </>
     )
 };
