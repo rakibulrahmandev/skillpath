03ce992c6e0457c1aac42a8715691bf628ec1726
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-27 12:12:14 +0600

    Title.jsx html structure done

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,7 +1,13 @@
-export default function Title() {
+export default function Title({title, text, link}) {
     return (
         <>
-            
+            <div>
+                <div>
+                    <h2>{title}</h2>
+                    <p>{text}</p>
+                </div>
+                <button className="secondary-btn">{link}</button>
+            </div>
         </>
     );
 };
