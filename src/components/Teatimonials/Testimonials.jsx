4f3cb7a3bf29fd70d1c18ff5f8261c0137e98bf0
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-28 13:05:16 +0600

    testimonials slider button done

--- src/components/Teatimonials/Testimonials.jsx
+++ src/components/Teatimonials/Testimonials.jsx
@@ -12,7 +12,7 @@ export default function Testimonials() {
                 <div className="container">
                     <Title title='Our Testimonials' text='Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.' link='View All' />
                     <Swiper modules={[Navigation, Autoplay]} spaceBetween={30}
-                    breakpoints={{640:{slidesPerView:1}, 768:{slidesPerView:1.5}, 1280:{slidesPerView:2.5}}} loop={true} className="mt-14 lg:mt-16">
+                    breakpoints={{640:{slidesPerView:1}, 768:{slidesPerView:1.5}, 1280:{slidesPerView:2.5}}} loop={true} navigation={{prevEl: '.prev-btn', nextEl: '.next-btn'}} autoplay={true} className="mt-14 lg:mt-16">
                         {testimonialsItems.map((item, idx) => (
                             <SwiperSlide key={idx} className="bg-white border border-white-95 p-8 lg:p-10 rounded-[10px] space-y-3">
                                 <p>{item?.text}</p>
@@ -28,11 +28,11 @@ export default function Testimonials() {
                             </SwiperSlide>
                         ))}
                     </Swiper>
-                    <div className="">
-                        <button className="">
+                    <div className="flex items-center justify-center mt-18 gap-5">
+                        <button className="bg-orange-70 w-12 h-12 flex items-center justify-center rounded-xl hover:bg-orange-75 transition-colors active:bg-orange-75/80 prev-btn">
                             <RiArrowLeftLine size={30} />
                         </button>
-                        <button>
+                        <button className="bg-orange-70 w-12 h-12 flex items-center justify-center rounded-xl hover:bg-orange-75 transition-colors active:bg-orange-75/80 next-btn">
                             <RiArrowRightLine size={30} />
                         </button>
                     </div>
