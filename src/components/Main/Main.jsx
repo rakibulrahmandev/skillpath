02b8e745f6706ef75825322a9b51f16d84062ad6
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-28 12:02:55 +0600

    Testimonial.jsx file mount in the Main.jsx file

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import Benefits from "../Benefits/Benefits";
 import Courses from "../Courses/Courses";
 import Home from "../Home/Home";
+import Testimonials from "../Teatimonials/Testimonials";
 
 export default function Main() {
     return (
@@ -8,6 +9,7 @@ export default function Main() {
             <Home />
             <Benefits />
             <Courses />
+            <Testimonials />
         </>
     );
 };
