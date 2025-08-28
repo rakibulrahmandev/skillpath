ab213f7aa8b0243e4e80c1cdf062fb41a29795dd
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-28 12:40:32 +0600

    Testimonials section card design done

--- src/components/Teatimonials/Testimonials.jsx
+++ src/components/Teatimonials/Testimonials.jsx
@@ -8,28 +8,30 @@ export default function Testimonials() {
             <section className="section">
                 <div className="container">
                     <Title title='Our Testimonials' text='Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.' link='View All' />
-                    <div className="">
+                    <div className="mt14 lg:mt-16">
                         {testimonialsItems.map((item, idx) => (
-                            <div key={idx} className="">
+                            <div key={idx} className="bg-white border border-white-95 p-8 lg:p-10 rounded-[10px] space-y-3">
                                 <p>{item?.text}</p>
-                                <div>
-                                    <div>
-                                        <img src={item?.img} alt={item?.author} width={50} height={50} />
+                                <div className="flex items-center justify-between flex-wrap pt-4 border-t border-t-orange-90 gap-4">
+                                    <div className="flex items-center gap-3">
+                                        <div>
+                                            <img src={item?.img} alt={item?.author} width={50} height={50} />
+                                        </div>
+                                        <p className="font-medium">{item?.author}</p>
                                     </div>
-                                    <p>{item?.author}</p>
+                                    <button className="secondary-btn">Read Full Story</button>
                                 </div>
-                                <button className="secondary-btn">Read Full Story</button>
                             </div>
                         ))}
                     </div>
-                    <div className="">
+                    {/* <div className="">
                         <button className="">
                             <RiArrowLeftLine size={30} />
                         </button>
                         <button>
                             <RiArrowRightLine size={30} />
                         </button>
-                    </div>
+                    </div> */}
                 </div>
             </section>
         </>
